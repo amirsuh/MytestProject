73ce5d5b94126ce36ebb05ad3925899ee81a2234
Amirsuhail Salim Hundekari <40598026+amirsuh@users.noreply.github.com>
2026-01-15 13:17:43 +0530

    #4 commit for 1.reducers,selctors

--- angular-ngrx-with-cart/src/app/app.ts
+++ angular-ngrx-with-cart/src/app/app.ts
@@ -2,6 +2,9 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Grocery } from "./component/grocery/grocery";
 import { Bucket } from "./component/bucket/bucket";
+import { Store } from '@ngrx/store';
+import { IGrocery } from './core/models/grocery.model';
+import { selectGroceriesByType } from './core/store/selector/grocery.selectors';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +14,13 @@ import { Bucket } from "./component/bucket/bucket";
 })
 export class App {
   protected readonly title = signal('angular-ngrx-with-cart');
+
+  // constructor(private store:Store<{grocery:IGrocery[]}>){
+  // //#1 should be same as app.config.ts reducer
+  // // this.groceries$ =
+  // this.store.select(selectGroceriesByType).subscribe(res=>{
+  //   console.log('data1 ',res)
+  // })
+
+// }
 }
--- angular-ngrx-with-cart/src/app/core/store/selector/grocery.selectors.ts
+++ angular-ngrx-with-cart/src/app/core/store/selector/grocery.selectors.ts
@@ -1,6 +1,26 @@
 // #5  to select types of grocery
 
+import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { IGrocery } from "../../models/grocery.model";
 
-
+// #5.0
 export const selectGroceries = (state:{grocery:IGrocery[]})=>state.grocery
+// #5.1 second type we can create selector
+export const selectGroceries2 = createFeatureSelector<IGrocery[]>('grocery')
+
+// #5.2 creaet selector with filter
+// export const selectGroceriesByType = createSelector(
+//   selectGroceries2,
+//   (state)=>{
+//     console.log('res')
+//     return state.filter(item=>item.type==='snacks')
+//   }
+// )
+
+// #5.3 creaet selector with filter with type above is custome
+export const selectGroceriesByType = (type:string)=>createSelector(
+  selectGroceries2,
+  (state)=>
+     state.filter(item=>item.type===type)
+
+)
